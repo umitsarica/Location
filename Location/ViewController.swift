575343d8b4a0ef9ca66d611a1e935591c41d9bbb
Ümit SARICA <usarica@gmail.com>
2024-01-29 10:44:28 +0300

    Initial Commit

---
+++ Location/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Location
+//
+//  Created by Hope on 29.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
